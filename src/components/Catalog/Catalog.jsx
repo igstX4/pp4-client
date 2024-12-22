6f64bd6acce590735a9c7483237e4e0187324d25
igstX4 <programx41@gmail.com>
2024-12-22 17:17:12 +0200

    asdasd

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -110,7 +110,7 @@ const Catalog = ({ setIsModalOpened }) => {
             
             if (isScrollable) {
                 e.preventDefault();
-                const scrollSpeed = 3;
+                const scrollSpeed = 6;
                 const delta = e.deltaY * scrollSpeed;
                 
                 container.scrollTo({
